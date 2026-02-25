020cca5f88733d304173bcc95153896025228bdf
Liammmmmmmm <lilefebv@student.42lyon.fr>
2026-02-25 13:13:56 +0100

    remove forgotten debug logs

--- src/tdr/LanguageService.cpp
+++ src/tdr/LanguageService.cpp
@@ -41,10 +41,7 @@ ParseResult SceneLanguageService::parse_file(const std::string& path)
 		std::ifstream in(path, std::ios_base::in);
 		if (!in.is_open()) throw TdrError("Cannot open file");
 
-		std::cout << "tttst" << std::endl;
 		auto tokens = lexer(in, errors);
-		std::cout << "tst" << std::endl;
-		print_tokens(tokens);
 		Node ast = parser(tokens, errors);
 
 		SceneSchema sch;
--- src/tdr/parser.cpp
+++ src/tdr/parser.cpp
@@ -48,7 +48,6 @@ Node parser(std::vector<Token>& list, ErrorCollector& errors)
 
 	std::function<std::unique_ptr<Node>()> parseNode = [&]() -> std::unique_ptr<Node>
 	{
-		std::cout << "OEOE" << std::endl;
 		auto eofError = [&]() -> std::unique_ptr<Node>
 		{
 			errors.report(TdrError(peek().line, peek().column, "Unexpected end of file"));
